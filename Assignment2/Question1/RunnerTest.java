eee2fab1626a44b374084803424f5d323fbe6945
Rahul Kumar <rahul.kumar@metacube.com>
2017-07-17 17:08:45 +0530

    Update RunnerTest.java

--- Assignment2/Question1/RunnerTest.java
+++ Assignment2/Question1/RunnerTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.notification.Failure;
 
 public class RunnerTest {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(TestSuite.class);
+      Result result = JUnitCore.runClasses(SuiteTest.class);
 
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
