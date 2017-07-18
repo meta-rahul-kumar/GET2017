37b090eb05f6078c9df14c467d92f04cd624b00f
Rahul Kumar <rahul.kumar@metacube.com>
2017-07-18 16:48:36 +0530

    Update GcdTest.java

--- Assignment3/Question1/GcdTest.java
+++ Assignment3/Question1/GcdTest.java
@@ -24,6 +24,18 @@ public class GcdTest {
 		expected = 6;
 		assertNotEquals(expected, output); // False case
 		
+		output = gcdFinder.gcd(2, 1);
+		expected = 1;
+		assertEquals(expected, output); // True case
+		
+		output = gcdFinder.gcd(12, 18);
+		expected = 6;
+		assertEquals(expected, output); // True case
+		
+		output = gcdFinder.gcd(100, 3);
+		expected = 4;
+		assertNotEquals(expected, output); // False case
+		
 		output = gcdFinder.gcd(-30, -60);
 		expected = 30;
 		assertEquals(expected, output); // True case
