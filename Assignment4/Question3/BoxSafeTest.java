e26843d161b21221a28db6cb76d363ec631651fa
Rahul Kumar <rahul.kumar@metacube.com>
2017-07-19 17:55:20 +0530

    Update BoxSafeTest.java

--- Assignment4/Question3/BoxSafeTest.java
+++ Assignment4/Question3/BoxSafeTest.java
@@ -14,39 +14,35 @@ public class BoxSafeTest {
 	@Test
 	public void BoxSafeTesting() {
 		NQueens obj = new NQueens();
-		int [][] board = new int [][] {
-            { 0,  0,  0,  0},
-             { 0,  0,  0,  0},
-             { 0,  0,  0,  0},
-             { 0,  0,  0,  0} };
+		int [][] board = new int [][] {{ 0,  0,  0,  0},
+            				       { 0,  0,  0,  0},
+            				       { 0,  0,  0,  0},
+             				       { 0,  0,  0,  0} };
         boolean result = obj.boxSafe(board , 1, 0, 4);
         boolean expected = true;
         assertEquals(expected, result); // True Case
         
-        board = new int [][] {
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0},
-            { 0,  0,  0,  0, 0, 0, 0, 0}};
+        board = new int [][] {{ 0,  0,  0,  0, 0, 0, 0, 0},
+          		      { 0,  0,  0,  0, 0, 0, 0, 0},
+                              { 0,  0,  0,  0, 0, 0, 0, 0},
+                              { 0,  0,  0,  0, 0, 0, 0, 0},
+                              { 0,  0,  0,  0, 0, 0, 0, 0},
+                              { 0,  0,  0,  0, 0, 0, 0, 0},
+                              { 0,  0,  0,  0, 0, 0, 0, 0},
+                              { 0,  0,  0,  0, 0, 0, 0, 0}};
         result = obj.boxSafe(board , 1, 0, 4);
         expected = true;
         assertEquals(expected, result); // True Case
         
-        board = new int [][] {
-            { 1,  0,  0},
-            { 0,  0,  0},
-            { 0,  0,  0}};
+        board = new int [][] {{ 1,  0,  0},
+			      { 0,  0,  0},
+			      { 0,  0,  0}};
          result = obj.boxSafe(board , 0, 0, 3);
          expected = true;
         assertNotEquals(expected, result); // False Case
         
-        board = new int [][] {
-            { 0,  0},
-            { 1,  0}};
+        board = new int [][] {{ 0,  0},
+          		      { 1,  0}};
         result = obj.boxSafe(board , 1, 0, 2);
         expected = true;
         assertNotEquals(expected, result); // False Case
